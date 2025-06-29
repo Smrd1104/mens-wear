94577025a086c7ca5b7888f2644161e86e42ac02
Smrd1104 <mriyasdeen1104@gmail.com>
2025-06-29 17:23:49 +0530

    hero section and font added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,7 +13,7 @@ import Navbar from "./components/Navbar"
 
 function App() {
   return (
-    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+    <div className="px-4 overflow-hidden">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
