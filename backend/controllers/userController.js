9177f0df42dc21c426b49a516ffaf3e9bf567212
Smrd1104 <mriyasdeen1104@gmail.com>
2025-07-02 14:54:23 +0530

    admin login setup over

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -88,7 +88,7 @@ const adminLogin = async (req, res) => {
         }
     } catch (error) {
         console.log(error)
-        res.json({ success: false, message: error.message})
+        res.json({ success: false, message: error.message })
 
     }
 
