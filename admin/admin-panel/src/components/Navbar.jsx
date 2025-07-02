dd0dd87e50577554d861785f920a59ab9c0b83ef
Smrd1104 <mriyasdeen1104@gmail.com>
2025-07-02 11:36:59 +0530

    admin page installation

---
+++ admin/admin-panel/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import { assets } from "../assets/admin_assets/assets"
+const Navbar = () => {
+    return (
+        <div className="flex items-center py-2 px-[4%] justify-between">
+            <img src={assets.logo} alt="" className="w-[max(10%,80px)]" />
+            <button className="bg-gray-600 text-white px-5 sm:px-7 py-2 sm:py-2 rounded-full text-xs sm:text-sm">Logout</button>
+        </div>
+    )
+}
+
+export default Navbar
