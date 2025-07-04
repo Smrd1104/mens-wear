8ae462ae37297a543c0c9976ee3a5acd31f3bab4
Smrd1104 <mriyasdeen1104@gmail.com>
2025-07-04 19:01:45 +0530

    server

--- backend/Server.js
+++ backend/Server.js
@@ -2,39 +2,120 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import connectCloudinary from "./config/cloudinary.js"
+import connectCloudinary from './config/cloudinary.js';
 import connectDB from './config/mongodb.js';
-import userRouter from "./routes/userRoute.js"
-import productRouter from "./routes/productRoute.js"
+import userRouter from './routes/userRoute.js';
+import productRouter from './routes/productRoute.js';
 import cartRouter from './routes/cartRoute.js';
-import orderRouter from "./routes/orderRoute.js"
+import orderRouter from './routes/orderRoute.js';
 
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
+
+// Connect to DB and Cloudinary
 connectDB();
 connectCloudinary();
+
 // Middlewares
 app.use(express.json());
 app.use(cors());
 
-
-// api endpoints
-
+// Routes
 app.use('/api/user', userRouter);
-app.use('/api/product', productRouter)
-app.use('/api/cart', cartRouter)
-app.use('/api/order', orderRouter)
+app.use('/api/product', productRouter);
+app.use('/api/cart', cartRouter);
+app.use('/api/order', orderRouter);
 
-// Basic Route
+// Default route
 app.get('/', (req, res) => {
-  res.send("API working");
+  res.send('API working Mohamed');
 });
 
 // Start server
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`✅ Server running on port ${PORT}`);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // server.js
+// import express from 'express';
+// import cors from 'cors';
+// import dotenv from 'dotenv';
+// import connectCloudinary from "./config/cloudinary.js"
+// import connectDB from './config/mongodb.js';
+// import userRouter from "./routes/userRoute.js"
+// import productRouter from "./routes/productRoute.js"
+// import cartRouter from './routes/cartRoute.js';
+// import orderRouter from "./routes/orderRoute.js"
+
+// dotenv.config();
+
+// const app = express();
+// const port = process.env.PORT || 5000;
+// connectDB();
+// connectCloudinary();
+// // Middlewares
+// app.use(express.json());
+// app.use(cors());
+
+
+// // api endpoints
+
+// app.use('/api/user', userRouter);
+// app.use('/api/product', productRouter)
+// app.use('/api/cart', cartRouter)
+// app.use('/api/order', orderRouter)
+
+// // Basic Route
+// app.get('/', (req, res) => {
+//   res.send("API working");
+// });
+
+// // Start server
+// app.listen(port, () => {
+//   console.log(`Server is running at http://localhost:${port}`);
+// });
 
 
