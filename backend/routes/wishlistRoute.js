7e6ad9e3eb94c448f78eb6d21a2a75d1b41868af
Smrd1104 <mriyasdeen1104@gmail.com>
2025-07-10 11:59:31 +0530

    changes

--- backend/routes/wishlistRoute.js
+++ backend/routes/wishlistRoute.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import { getWishlist, addToWishlist, removeFromWishlist } from '../controllers/wishlistController.js';
-import authUser from '../middleware/authUser.js';
+import authUser from '../middleware/auth.js';
 
 const wishlistRouter = express.Router();
 
-wishlistRouter.get('/', authUser, getWishlist);         // Use decoded userId from token
-wishlistRouter.post('/', authUser, addToWishlist);
-wishlistRouter.delete('/', authUser, removeFromWishlist);
+wishlistRouter.get('/:userId', authUser, authUser, getWishlist);         // Use decoded userId from token
+wishlistRouter.post('/', authUser, authUser, addToWishlist);
+wishlistRouter.delete('/', authUser, authUser, removeFromWishlist);
 
 export default wishlistRouter;
